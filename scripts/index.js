433f986dd74bb18ac38a9835f8aac0f3370c61ca
SumAmSum <57662447+SumAmSum@users.noreply.github.com>
2019-11-12 15:27:18 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,13 @@
 let audioPlaying = true,
 backgroundAudio,browser;
 browser = navigator.userAgent.toLowerCase();
-$('<audio class="audio1" src="../uploads/jezus_kristus_kak_smo_zejni.mp3" loop></audio>').prependTo('body');
+$('<audio class="audio1" src="https://sumamsum.github.io/jezus/uploads/jezus_kristus_kak_smo_zejni.mp3" loop></audio>').prependTo('body');
 if (!browser.indexOf('firefox') > -1) {
-    $('<embed id="background-audio" src="../uploads/jezus_kristus_kak_smo_zejni.mp3" autostart="1"></embed>').prependTo('body');
+    $('<embed id="background-audio" src="https://sumamsum.github.io/jezus/uploads/jezus_kristus_kak_smo_zejni.mp3" autostart="1"></embed>').prependTo('body');
 
  backgroundAudio = setInterval(function() {
       $("#background-audio").remove();
-       $('<embed id="background-audio" src="../uploads/jezus_kristus_kak_smo_zejni.mp3"></embed>').prependTo('body');
+       $('<embed id="background-audio" src="https://sumamsum.github.io/jezus/uploads/jezus_kristus_kak_smo_zejni.mp3"></embed>').prependTo('body');
       }, 36000);
       //36000 is the duration of your audio which in this case 36 seconds.
 }
