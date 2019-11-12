bd822d2740df051fd160d117f49c30b4c9dc6588
SumAmSum <57662447+SumAmSum@users.noreply.github.com>
2019-11-12 14:57:10 +0100

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+//index.js
+let audioPlaying = true,
+backgroundAudio,browser;
+browser = navigator.userAgent.toLowerCase();
+$('<audio class="audio1" src="jezus_kristus_kak_smo_zejni.mp3" loop></audio>').prependTo('body');
+if (!browser.indexOf('firefox') > -1) {
+    $('<embed id="background-audio" src="jezus_kristus_kak_smo_zejni.mp3" autostart="1"></embed>').prependTo('body');
+
+ backgroundAudio = setInterval(function() {
+      $("#background-audio").remove();
+       $('<embed id="background-audio" src="jezus_kristus_kak_smo_zejni.mp3"></embed>').prependTo('body');
+      }, 36000);
+      //36000 is the duration of your audio which in this case 36 seconds.
+}
